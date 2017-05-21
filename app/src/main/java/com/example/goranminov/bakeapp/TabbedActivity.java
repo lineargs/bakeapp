77bc431e3f50a0735491fc5a926fa758e1d900e5
Goran Minov <goran.s.minov@gmail.com>
2017-05-21 19:04:42 +0100

    Added ExoPlayer

--- app/src/main/java/com/example/goranminov/bakeapp/TabbedActivity.java
+++ app/src/main/java/com/example/goranminov/bakeapp/TabbedActivity.java
@@ -16,10 +16,11 @@
 
 package com.example.goranminov.bakeapp;
 
+import android.content.Context;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -33,17 +34,28 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
 
 import com.example.goranminov.bakeapp.data.BakingContract;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class TabbedActivity extends AppCompatActivity
-implements LoaderManager.LoaderCallbacks<Cursor>{
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -60,6 +72,7 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
      */
     private ViewPager mViewPager;
     private Uri mUri;
+    private int mCurrentItem = 0;
 
     private static final int LOADER_ID = 101;
 
@@ -86,16 +99,13 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
             throw new NullPointerException("URI cannot be null");
         } else {
             mUri = getIntent().getData();
-            Log.v("Uri: ", getIntent().getData().toString());
         }
         if (getIntent().hasExtra("position")) {
-            Log.v("position ", getIntent().getStringExtra("position"));
+            mCurrentItem = Integer.parseInt(getIntent().getStringExtra("position"));
         }
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-        int position = Integer.parseInt(getIntent().getStringExtra("position")) - 1;
-        mViewPager.setCurrentItem(2);
         getSupportLoaderManager().initLoader(LOADER_ID, null, this);
     }
 
@@ -120,7 +130,8 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
         int id = loader.getId();
         switch (id) {
             case LOADER_ID:
-           mSectionsPagerAdapter.swapCursor(data);
+                mSectionsPagerAdapter.swapCursor(data);
+                mViewPager.setCurrentItem(mCurrentItem);
                 break;
             default:
                 throw new RuntimeException("Loader not implemented: " + id);
@@ -131,7 +142,6 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
     public void onLoaderReset(Loader<Cursor> loader) {
 
     }
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -140,8 +150,12 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
          * The fragment argument representing the section number for this
          * fragment.
          */
+        private SimpleExoPlayer mExoPlayer;
+        private SimpleExoPlayerView mPlayerView;
+
+        private static final String TAG = PlaceholderFragment.class.getSimpleName();
         private static final String ARG_DESCRIPTION = "description";
-        private static final String ARG_POSITION = "position";
+        private static final String ARG_VIDEO = "video";
 
         public PlaceholderFragment() {
         }
@@ -150,11 +164,11 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int position, String description) {
+        public static PlaceholderFragment newInstance(String description, String mediaUri) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putString(ARG_DESCRIPTION, description);
-            args.putInt(ARG_POSITION, position);
+            args.putString(ARG_VIDEO, mediaUri);
             fragment.setArguments(args);
             return fragment;
         }
@@ -164,8 +178,26 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.playerView);
+            mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource
+                    (getResources(), R.drawable.question_mark));
             textView.setText(getArguments().getString(ARG_DESCRIPTION));
             return rootView;
+        }
+
+        private void initializePlayer(Uri uri) {
+            if (mExoPlayer == null) {
+                TrackSelector trackSelector = new DefaultTrackSelector();
+                LoadControl loadControl = new DefaultLoadControl();
+                mExoPlayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(getContext()),
+                        trackSelector, loadControl);
+                mPlayerView.setPlayer(mExoPlayer);
+                MediaSource mediaSource = new ExtractorMediaSource(uri,
+                        new DefaultDataSourceFactory(getContext(), Util.getUserAgent(getContext(), "BakeApp")),
+                        new DefaultExtractorsFactory(), null, null);
+                mExoPlayer.prepare(mediaSource);
+//                mExoPlayer.setPlayWhenReady(true);
+            }
         }
     }
 
@@ -173,10 +205,10 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
         private Cursor mCursor;
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
@@ -184,9 +216,10 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
         public Fragment getItem(int position) {
             mCursor.moveToPosition(position);
             String description = mCursor.getString(TabbedActivity.INDEX_DESCRIPTION);
+            String mediaUri = mCursor.getString(TabbedActivity.INDEX_VIDEO);
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position, description);
+            return PlaceholderFragment.newInstance(description, mediaUri);
         }
 
         @Override
@@ -195,7 +228,7 @@ implements LoaderManager.LoaderCallbacks<Cursor>{
             return mCursor.getCount();
         }
 
-        public void swapCursor(Cursor cursor) {
+        void swapCursor(Cursor cursor) {
             mCursor = cursor;
             notifyDataSetChanged();
         }
