0bf958250c1f7884afa48bf76ee74cad490eb73d
Goran Minov <goran.s.minov@gmail.com>
2017-06-03 18:45:59 +0100

    Added IdlingResource Test

--- app/src/main/java/com/example/goranminov/bakeapp/MainActivity.java
+++ app/src/main/java/com/example/goranminov/bakeapp/MainActivity.java
@@ -18,10 +18,15 @@ package com.example.goranminov.bakeapp;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.goranminov.bakeapp.idlingResource.SimpleIdlingResource;
 import com.example.goranminov.bakeapp.utils.RetrofitCall;
 
 
@@ -30,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
 
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+        }
+        return mIdlingResource;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +54,18 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         if (sharedPreferences.getBoolean(getString(R.string.pref_first_key), true)) {
+            getIdlingResource();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (sharedPreferences.getBoolean(getString(R.string.pref_first_key), true)) {
             RetrofitCall.getRecipes(this);
             sharedPreferences.edit().putBoolean(getString(R.string.pref_first_key), false).apply();
         }
     }
+
 }
